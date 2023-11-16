cd0b0dd2dfde9e9fea08e3306a5a6a8f49b3ccfd
brenonogueirasilva <brenogueirasilva@gmail.com>
2023-11-16 18:27:26 -0300

    <feat> [integrate_api.py] add Docstring and annotation

--- src/classes/integrate_api.py
+++ src/classes/integrate_api.py
@@ -1,5 +1,15 @@
 class IntegrateApi:
-    def params_to_dict(self, params):
+    """
+    A class for integrating with an API.
+    """
+    def params_to_dict(self, params) -> dict:
+        """
+        Converts parameters from a Pydantic model to a dictionary.
+        Args:
+            params: Pydantic model containing parameters.
+        Returns:
+            dict: A dictionary containing non-None values from the Pydantic model.
+        """
         dict_params = { 
                 'name' : params.name,               
                 'year' : params.year,              
@@ -14,13 +24,27 @@ class IntegrateApi:
         dict_params = dict(dict_params)
         return dict_params
 
-    def list_params_to_dict(self, ls_params):
+    def list_params_to_dict(self, ls_params: list) -> list:
+        """
+        Converts a list of Pydantic models to a list of dictionaries.
+        Args:
+            ls_params (list): List of Pydantic models.
+        Returns:
+            list: List of dictionaries containing non-None values from each Pydantic model.
+        """
         ls_treated_params = []
         for element in ls_params:
             ls_treated_params.append(self.params_to_dict(element))
         return ls_treated_params
 
-    def update_params_to_dict(self, update_params):
+    def update_params_to_dict(self, update_params: dict) -> dict:
+        """
+        Converts update parameters from Pydantic models to a dictionary.
+        Args:
+            update_params: Pydantic model containing update parameters.
+        Returns:
+            dict: A dictionary containing 'new_document' and 'query' keys with corresponding values.
+        """
         update_dict = {}
         update_dict["new_document"] = self.params_to_dict( update_params.new_document )
         update_dict["query"] = self.params_to_dict( update_params.query)
